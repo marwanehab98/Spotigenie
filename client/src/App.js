75af61f0702c7c0f691e006b04e5b33ab997fd75
Marwan Ehab <marwan.el.sheikh14@gmail.com>
2023-03-07 14:14:08 +0200

    tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/dashboard/dashboard';
+import Dashboard from './pages/home/home';
 import { NextUIProvider } from '@nextui-org/react';
+
 
 function App() {
   return (
